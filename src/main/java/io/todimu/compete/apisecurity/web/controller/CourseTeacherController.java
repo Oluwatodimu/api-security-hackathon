f0bd38725a35ffe4983dcb3598c28d3f97638091
todimuisewon <todimuisewon@gmail.com>
2023-07-05 19:46:22 +0100

    added docker

--- src/main/java/io/todimu/compete/apisecurity/web/controller/CourseTeacherController.java
+++ src/main/java/io/todimu/compete/apisecurity/web/controller/CourseTeacherController.java
@@ -2,12 +2,14 @@ package io.todimu.compete.apisecurity.web.controller;
 
 import io.todimu.compete.apisecurity.dto.request.AssignTeacherToCourseRequest;
 import io.todimu.compete.apisecurity.service.CourseTeacherService;
+import io.todimu.compete.apisecurity.utils.AuthoritiesConstants;
 import io.todimu.compete.apisecurity.utils.ResponseConstants;
 import io.todimu.compete.apisecurity.web.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,7 @@ public class CourseTeacherController {
     private final CourseTeacherService courseTeacherService;
 
     @PostMapping
+    @PreAuthorize(AuthoritiesConstants.ADMIN)
     public ResponseEntity<BaseResponse> assignTeacherToCourse(@RequestBody @Valid AssignTeacherToCourseRequest request) {
         log.info("assigning teacher {} to course", request.getTeacherEmail());
         UUID response = courseTeacherService.assignTeacherToCourse(request);
